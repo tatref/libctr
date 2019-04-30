2303ecf9ce503ccbf70f0f52ec9805c617d1e42a
tatref <tatref@github.com>
2019-05-01 00:26:14 +0200

    pass language as argument

--- create_libs.sh
+++ create_libs.sh
@@ -21,64 +21,67 @@ else
 fi
 
 
-# graphviz
-echo "graphviz..."
-
-mkdir -p graphviz
-rm -rf graphviz/*
-for ksy in kaitai-struct/*.ksy
-do
-  echo $ksy
-  $KSC $ksy \
-    --target graphviz \
-    --import-path kaitai-struct/ \
-    --outdir graphviz
-done
-if type dot > /dev/null 2>&1
-then
-  for dot in graphviz/*.dot
+graphviz() {
+  echo "graphviz..."
+  
+  mkdir -p graphviz
+  rm -rf graphviz/*
+  for ksy in kaitai-struct/*.ksy
   do
-    echo $dot
-    dot -Tpng $dot -o ${dot%%.dot}.png
+    echo $ksy
+    $KSC $ksy \
+      --target graphviz \
+      --import-path kaitai-struct/ \
+      --outdir graphviz
   done
-else
-  echo "dot (graphviz) not found, skipping .png generation"
-fi
+  if type dot > /dev/null 2>&1
+  then
+    for dot in graphviz/*.dot
+    do
+      echo $dot
+      dot -Tpng $dot -o ${dot%%.dot}.png
+    done
+  else
+    echo "dot (graphviz) not found, skipping .png generation"
+  fi
+}
 
 
-# csharp
-echo "csharp..."
+csharp() {
+  echo "csharp..."
+  
+  mkdir -p csharp/ctr
+  rm -rf csharp/ctr/*
+  
+  for ksy in kaitai-struct/*.ksy
+  do
+    echo $ksy
+    $KSC $ksy \
+  	  --target csharp \
+  	  --import-path kaitai-struct/ \
+  	  --outdir csharp/ctr \
+  	  --dotnet-namespace ctr
+  done
+}
 
-mkdir -p csharp/ctr
-rm -rf csharp/ctr/*
 
-for ksy in kaitai-struct/*.ksy
-do
-  echo $ksy
-  $KSC $ksy \
-	  --target csharp \
-	  --import-path kaitai-struct/ \
-	  --outdir csharp/ctr \
-	  --dotnet-namespace ctr
-done
-
-
-# python
-echo "python..."
-
-mkdir -p python/ctr
-rm -rf python/ctr/*
-touch python/ctr/__init__.py
-
-for ksy in kaitai-struct/*.ksy
-do
-  echo $ksy
-  $KSC $ksy \
-	  --target python \
-	  --import-path kaitai-struct/ \
-	  --outdir python/ctr \
-	  --python-package ctr
-done
+python() {
+  echo "python..."
+  
+  mkdir -p python/ctr
+  rm -rf python/ctr/*
+  touch python/ctr/__init__.py
+  
+  for ksy in kaitai-struct/*.ksy
+  do
+    echo $ksy
+    $KSC $ksy \
+  	  --target python \
+  	  --import-path kaitai-struct/ \
+  	  --outdir python/ctr \
+  	  --python-package ctr
+  done
+}
 
 
 # TODO: perl
@@ -89,4 +92,14 @@ done
 # TODO: lua
 # TODO: ruby
 # TODO: javascript
+
+
+if [ $# -eq 1 ]
+then
+  $1
+else
+  graphviz
+  python
+  csharp
+fi
 
